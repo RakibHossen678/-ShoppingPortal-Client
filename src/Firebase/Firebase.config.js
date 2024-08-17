15476b5ccfdaceff86215a815f3df01f210d7f57
RakibHossen678 <rh491464@gmail.com>
2024-08-17 12:05:06 +0600

    add environment variable

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,18 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBkU0b_5jRcl9GjW7Uqdpozk4ABhzVUYQM",
-  authDomain: "shoppingportal-bbd75.firebaseapp.com",
-  projectId: "shoppingportal-bbd75",
-  storageBucket: "shoppingportal-bbd75.appspot.com",
-  messagingSenderId: "750317688245",
-  appId: "1:750317688245:web:5939f06ea6fed66cea75fc",
+  apiKey: import.meta.env.DB_APIKEY,
+  authDomain: import.meta.env.DB_AUTHDOMAIN,
+  projectId: import.meta.env.DB_PROJECTID,
+  storageBucket: import.meta.env.DB_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.DB_MESSAGINGSENDERID,
+  appId: import.meta.env.DB_APPID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth=getAuth(app)
+const auth = getAuth(app);
 export default auth;
